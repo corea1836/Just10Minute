a3b0e719078a7e8d615e9a152a21fa60b41f6b4f
HONGJIBEOM <corea1836@naver.com>
2024-04-16 22:24:43 +0900

    [config] test schema 작성 및 h2 연결 확인

--- src/main/java/com/jb/Just10Minute/testRunner.java
+++ src/main/java/com/jb/Just10Minute/testRunner.java
@@ -5,10 +5,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
+import java.util.List;
+import java.util.Map;
 
 @Component
 @Slf4j
@@ -17,12 +20,17 @@ public class testRunner implements ApplicationRunner {
     @Autowired
     private DataSource dataSource;
 
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
     @Override
     public void run(ApplicationArguments args) throws Exception {
         Connection con = dataSource.getConnection();
-
+        log.info("DB Connection Pool : {}", dataSource.getClass());
         log.info("url : {}", con.getMetaData().getURL());
-        log.info("url : {}", con.getMetaData().getUserName());
+        log.info("userName : {}", con.getMetaData().getUserName());
 
+        List<Map<String, Object>> maps = jdbcTemplate.queryForList("SELECT * FROM test_table");
+        maps.stream().forEach(m -> log.info(m.toString()));
     }
 }
